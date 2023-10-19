4178b6829a383537ed8f2feefe34b958285d6c5a
Joaquin Bejar <jbejar@swissblock.net>
2023-10-19 19:35:51 +0200

    Implement Config class

--- cmake/nlohmann_json.cmake
+++ cmake/nlohmann_json.cmake
@@ -6,7 +6,6 @@ FetchContent_Declare(nlohmann_json
         GIT_TAG v3.11.2
 )
 FetchContent_MakeAvailable(nlohmann_json)
-message(STATUS "NLOHMANN_JSON_SOURCE_DIR ${nlohmann_json_SOURCE_DIR}")
 set(NLOHMANN_JSON_SOURCE_DIR ${nlohmann_json_SOURCE_DIR} CACHE INTERNAL "")
 set(NLOHMANN_JSON_INCLUDE ${NLOHMANN_JSON_SOURCE_DIR}/single_include CACHE INTERNAL "")
 message(STATUS "NLOHMANN_JSON_INCLUDE ${NLOHMANN_JSON_INCLUDE}")

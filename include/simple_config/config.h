db347c1de28c7a39528858f4479cbb5549d2df67
Joaquin Bejar <jbejar@swissblock.net>
2023-10-19 18:25:42 +0200

    first commit

---
+++ include/simple_config/config.h
@@ -0,0 +1,17 @@
+//
+// Created by Joaquin Bejar Garcia on 19/10/23.
+//
+
+#ifndef SIMPLE_CONFIG_CONFIG_H
+#define SIMPLE_CONFIG_CONFIG_H
+
+#include <iostream>
+#include <mutex>
+
+namespace simple_config {
+
+
+    
+}
+
+#endif //SIMPLE_CONFIG_CONFIG_H
---
+++ src/config.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Joaquin Bejar Garcia on 19/10/23.
+//
+
+#include "simple_config/config.h"
+
+namespace simple_config {
+
+}
